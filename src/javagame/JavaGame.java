37e3044c06f292a86dfc615dd89cc101b7df42e7
DaSammich <samsonchua1990@gmail.com>
2014-08-24 18:18:36 -0700

    after episode 4

--- src/javagame/JavaGame.java
+++ src/javagame/JavaGame.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package javagame;
 
 import java.awt.Graphics;
@@ -12,34 +11,51 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 
-
 public class JavaGame extends JFrame {
-    
+
     int x, y;
     private Image dbImage;
     private Graphics dbg;
-    
+
     public class AL extends KeyAdapter {
+
         public void keyPressed(KeyEvent e) {
             int keyCode = e.getKeyCode();
-            if(keyCode == e.VK_LEFT) {
-               x-=5;
+            if (keyCode == e.VK_LEFT) {
+                if (x <= 0) {
+                    x = 0;
+                } else {
+                    x -= 5;
+                }
             }
-            if(keyCode == e.VK_RIGHT) {
-               x+=5;
+            if (keyCode == e.VK_RIGHT) {
+                if (x >= 230) {
+                    x = 230;
+                } else {
+                    x += 5;
+                }
             }
-            if(keyCode == e.VK_UP) {
-                y-=5;
+            if (keyCode == e.VK_UP) {
+                if (y <= 20) {
+                    y = 20;
+                } else {
+                    y -= 5;
+                }
             }
-            if(keyCode == e.VK_DOWN) {
-                y+=5;
+            if (keyCode == e.VK_DOWN) {
+                if (y >= 230) {
+                    y = 230;
+                } else {
+                    y += 5;
+                }
             }
         }
+
         public void keyReleased(KeyEvent e) {
-            
+
         }
     }
-    
+
     public JavaGame() {
         addKeyListener(new AL());
         setTitle("Java Game");
@@ -47,18 +63,18 @@ public class JavaGame extends JFrame {
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         x = 150;
         y = 150;
     }
-    
+
     public void paint(Graphics g) {
         dbImage = createImage(getWidth(), getHeight());
         dbg = dbImage.getGraphics();
         paintComponent(dbg);
-        g.drawImage(dbImage, 0,0, this);
+        g.drawImage(dbImage, 0, 0, this);
     }
-    
+
     public void paintComponent(Graphics g) {
         g.fillOval(x, y, 15, 15);
         repaint();
@@ -67,5 +83,5 @@ public class JavaGame extends JFrame {
     public static void main(String[] args) {
         new JavaGame();
     }
-    
+
 }
